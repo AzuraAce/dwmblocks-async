629c4cf8924c18c838baecf2e945a7c618dfc1e1
AzuraAce <oliverjbmub@gmail.com>
2025-04-03 14:50:13 +0200

    removed unused status

--- config.h
+++ config.h
@@ -18,15 +18,8 @@
 
 // Define blocks for the status feed as X(icon, cmd, interval, signal).
 #define BLOCKS(X)             \
-/*     X("", "sb-mail", 600, 1)  \ */
     X("", "/home/oliver/scripts/dwm/dwm-brightness get", 0, 2)   \
-    /* X("", "sb-disk", 1800, 3) \
-    X("", "sb-memory", 10, 4) \
-    X("", "sb-loadavg", 5, 5) \
-    X("", "sb-mic", 0, 6)     \
-    X("", "sb-record", 0, 7)  \
-    X("", "sb-volume", 0, 8)  \ */
-    X("", "acpi | awk '{print $3 $4}' | sed 's/,/ /g'", 5, 9) \ // Battery status
+    X("", "acpi | awk '{print $3 $4}' | sed 's/,/ /g'", 5, 9) \
     X("", "date '+%a, %d %b %Y %H:%M'", 1, 10)
 
 #endif  // CONFIG_H
